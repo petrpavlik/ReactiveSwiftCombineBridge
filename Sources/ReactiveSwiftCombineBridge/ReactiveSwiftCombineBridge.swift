74380c535694f1bf20c6191af4058b11959c2ed2
Petr Pavlik <petrpavlik@me.com>
2020-10-18 17:07:26 +0200

    initial commit

--- Sources/ReactiveSwiftCombineBridge/ReactiveSwiftCombineBridge.swift
+++ Sources/ReactiveSwiftCombineBridge/ReactiveSwiftCombineBridge.swift
@@ -1,3 +1,74 @@
+
+import Combine
+import ReactiveSwift
+
 struct ReactiveSwiftCombineBridge {
     var text = "Hello, World!"
+}
+
+@available(iOS 13.0, *)
+public extension AnyPublisher {
+
+    func asSignalProducer() -> SignalProducer<Output, Failure> {
+        
+        let reference = self.share()
+
+        return SignalProducer { (signal, lifetime) in
+            let cancellable = reference.handleEvents(receiveCancel: {
+                signal.sendInterrupted()
+            }).sink(
+                receiveCompletion: { completion in
+                    switch completion {
+                    case .finished:
+                        signal.sendCompleted()
+                    case .failure(let error):
+                        signal.send(error: error)
+                    }
+                },
+                receiveValue: { value in
+                    signal.send(value: value)
+                }
+            )
+
+            lifetime.observeEnded {
+                cancellable.cancel()
+            }
+        }
+    }
+}
+
+@available(iOS 13.0, *)
+public extension Signal {
+    func asPublisher() -> AnyPublisher<Value, Error> {
+
+        let x = PassthroughSubject<Value, Error>()
+
+        self.on(event: { (event) in
+            switch event {
+            case .value(let value):
+                x.send(value)
+            case .failed(let error):
+                x.send(completion: .failure(error))
+            case .completed:
+                x.send(completion: .finished)
+            case .interrupted:
+                x.send(completion: .finished)
+            }
+        }).observeCompleted {
+            //
+        }
+
+        return x.eraseToAnyPublisher()
+    }
+}
+
+@available(iOS 13.0, *)
+extension SignalProducer {
+    func startAsFuture() -> Future<Value, Error> {
+        Future { (promise) in
+            self.take(first: 1).startWithResult { (result) in
+                promise(result)
+            }
+        }
+    }
 }
--- Tests/ReactiveSwiftCombineBridgeTests/ReactiveSwiftCombineBridgeTests.swift
+++ Tests/ReactiveSwiftCombineBridgeTests/ReactiveSwiftCombineBridgeTests.swift
@@ -1,15 +1,114 @@
 import XCTest
 @testable import ReactiveSwiftCombineBridge
+import ReactiveSwift
+import Combine
 
+@available(iOS 13.0, *)
 final class ReactiveSwiftCombineBridgeTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(ReactiveSwiftCombineBridge().text, "Hello, World!")
+
+    func testAsFuture() {
+        let signalProducer: SignalProducer<Bool, Error> = SignalProducer { (signal, _) in
+            signal.send(value: true)
+        }
+
+        let valueExpectation = expectation(description: "value")
+        let completedExpectation = expectation(description: "completed")
+
+        let subscription = signalProducer.startAsFuture().sink { (completion) in
+            switch completion {
+            case .finished:
+                completedExpectation.fulfill()
+            case .failure(_):
+                break
+            }
+        } receiveValue: { (value) in
+            XCTAssert(value)
+            valueExpectation.fulfill()
+        }
+
+        wait(for: [valueExpectation, completedExpectation], timeout: 1)
+
+        subscription.cancel()
+    }
+
+    func testAsSignal() {
+
+        class Counter {
+
+            @Published private(set) var count = 0
+
+            func incerement() {
+                count += 1
+            }
+        }
+
+        let counter = Counter()
+
+        let countIs1Expecation = expectation(description: "count is 1")
+        let disposedExpectation = expectation(description: "disposed")
+
+        let disposable = counter.$count.eraseToAnyPublisher().asSignal()
+            .on(disposed: {
+                disposedExpectation.fulfill()
+            })
+            .observeValues({ (count) in
+                if count == 1 {
+                    countIs1Expecation.fulfill()
+                } else {
+                    XCTFail()
+                }
+            })
+
+        wait(for: [countIs1Expecation], timeout: 1)
+
+        disposable?.dispose()
+
+        wait(for: [disposedExpectation], timeout: 1)
+    }
+
+    func testAsSignalProducer() {
+
+        class Counter {
+
+            @Published private(set) var count = 0
+
+            func incerement() {
+                count += 1
+            }
+        }
+
+        let counter = Counter()
+
+        let countIs0Expecation = expectation(description: "count is 0")
+        let countIs1Expecation = expectation(description: "count is 1")
+        let disposedExpectation = expectation(description: "disposed")
+
+        let disposable = counter.$count.eraseToAnyPublisher().asSignalProducer()
+            .on(disposed: {
+                disposedExpectation.fulfill()
+            })
+            .startWithValues { (count) in
+                if count == 0 {
+                    countIs0Expecation.fulfill()
+                } else if count == 1 {
+                    countIs1Expecation.fulfill()
+                } else {
+                    XCTFail()
+                }
+        }
+
+        wait(for: [countIs0Expecation], timeout: 1)
+
+        counter.incerement()
+
+        wait(for: [countIs1Expecation], timeout: 1)
+
+        disposable.dispose()
+
+        wait(for: [disposedExpectation], timeout: 1)
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testExample", testAsFuture),
     ]
 }
